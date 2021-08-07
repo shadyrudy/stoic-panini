ba8be98d0d4d8e948798af60d761436f73b09513
Rudy Rodarte <rudy@outlook.com>
2021-08-07 14:08:07 -0500

    Updated gitignore First shot at adding react

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDom from 'react-dom'
+
+function App() {
+    const [count, setCount] = React.useState(0);
+
+    return (
+        <div>
+            Hello from React!
+            <button onClick={() => setCount(count + 1)}>Click Me! {count}</button>
+        </div>
+    )
+}
+ReactDom.render(<App></App>, document.getElementById('app'))
